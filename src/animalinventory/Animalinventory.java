9b55e265cceedf884d794bf6d667b8e44d9abde1
Mehdi O. Hadj-Merabet <18150@columbushs.com>
2017-12-21 12:51:54 -0500

    first commit

---
+++ src/animalinventory/Animalinventory.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animalinventory;
+
+/**
+ *
+ * @author 18150
+ */
+public class Animalinventory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       int dogs = 10;
+       int cats = 28;
+       int pYtHoNs = 1;
+       int rats = 17;
+       int totalAnimals = (10 + 28 + 1 + 17);
+       char dayOfWeek = 'm';
+       String dogLabel = "puppies : ";
+       String catLabel = "'cats : ";
+       String snakeLabel = "Snakes : ";
+       String ratLabel = "Mice : " ;
+       String totalLabel = "All Animals : ";
+       String dayLabel = "day : ";
+       
+       
+       System.out.print(dayLabel);
+       System.out.println(dayOfWeek);
+       System.out.print(dogLabel);
+       System.out.println(dogs);
+       System.out.print(catLabel);
+       System.out.println(cats);
+       System.out.print(snakeLabel);
+       System.out.println(pYtHoNs);
+       System.out.print(ratLabel);
+       System.out.println(rats);
+       System.out.print(totalLabel);
+       System.out.println(totalAnimals);
+       
+       
+       
+              
+        
+        
+        
+    }
+    
+}
